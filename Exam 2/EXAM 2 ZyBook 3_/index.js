14da21b37780d8aaaad580f964b666ccce0e89a4
Patricker75 <biet.vui@gmail.com>
2023-10-12 13:48:42 -0500

    finished exam

--- Exam 2/EXAM 2 ZyBook 3_/index.js
+++ Exam 2/EXAM 2 ZyBook 3_/index.js
@@ -1,5 +1,15 @@
 function calcWordFrequencies() {
-  let input = propmpt();
+  let input = prompt("");
+  let words = input.split(" ");
+  
+  for(let word of words) {
+    let count = 0;
+    for (let i=0; i < words.length; i++) {
+      if (word === words[i]) {
+        count++;
+      }
+    }
 
-  let words = 
+    console.log(word + ' ' + count);
+  }
 }
---
+++ Exam 2/EXAM 2 ZyBook 5. (3 attempts)/index.js
@@ -0,0 +1,6 @@
+function displayElements(){
+
+   const userArray = prompt(); // Code will be tested with other array values as well
+   
+   console.log(userArray[1] + ' ' + userArray[4])
+}
